f3cb74b7a1f9b38bc60179c4afdae2433955ac38
Kelompok4410 <75293136+Kelompok4410@users.noreply.github.com>
2020-12-02 07:36:59 +0000

    Create construktor.php

---
+++ construktor.php
@@ -0,0 +1,24 @@
+<?php 
+
+//class Hewan
+class Hewan{
+  //property
+  var $nama; 
+  var $warna; 
+  
+  function __construct() {
+    echo "hewan gajah";
+}
+
+  //method hewan
+  function tampilkan_nama() {
+      return "Nama saya gajah";
+  }
+
+}
+//instansiasi class hewan
+$hewan = New hewan();
+
+//memanggil method tampilkan_Nama dari class hewan
+echo $hewan->tampilkan_nama();
+?>
