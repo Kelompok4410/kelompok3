69769950dd5f7de1cc8b8ecfe6a40a1f7af85b34
Kelompok4410 <75293136+Kelompok4410@users.noreply.github.com>
2020-12-02 07:23:01 +0000

    Create class.php

--- class.php
+++ class.php
@@ -1,1 +1,25 @@
-isi scrip disini
+<?php 
+
+//class Hewan
+class Hewan{
+  //property
+  var $nama; 
+  var $warna; 
+  
+  function __construct() {
+    echo "hewan ayam";
+}
+
+  //method hewan
+  function tampilkan_nama() {
+      return "Nama saya Ayam";
+  }
+
+}
+//instansiasi class hewan
+$hewan = New hewan();
+
+//memanggil method tampilkan_Nama dari class hewan
+echo $hewan->tampilkan_nama();
+?>
+
